98284e4f612986c3281ced8b0b72df14c0f76e83
gavin <gavin.r.barnard@gmail.com>
2017-01-12 22:22:35 -0500

    fixes

--- pkt2wire.ps1
+++ pkt2wire.ps1
@@ -7,5 +7,5 @@ $myData = $myInput | Where {$_ -match "^        0x"}
 New-Item -Name $outFile -ItemType File
 
 $myData | % {
-    $_ -replace "        0x","00" -replace ":  "," "  -replace " ([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])",' $1 $2' | Out-File -Append -encoding ascii $outFile 
+    $_ -replace "        0x","00" -replace ":  "," "  -replace " ([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])",' $1 $2' | Out-File -Append -encoding ascii $outFile
 }
