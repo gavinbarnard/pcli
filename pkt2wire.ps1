61420b8fb4daef5e3de8cf165db0bfd6a1a07e99
Gavin Barnard <gavinbarnard@vmware.com>
2019-12-26 14:16:32 -0500

    tab or space

--- pkt2wire.ps1
+++ pkt2wire.ps1
@@ -2,10 +2,10 @@ param($inputFile,$outFile)
 
 $myInput = Get-Content $inputFile
 
-$myData = $myInput | Where {$_ -match "^        0x"}
+$myData = $myInput | Where {$_ -match "^(       |\t)0x"}
 
 New-Item -Name $outFile -ItemType File
 
 $myData | % {
-    $_ -replace "        0x","00" -replace ":  "," "  -replace " ([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])",' $1 $2' | Out-File -Append -encoding ascii $outFile
+    $_ -replace "(        |\t)0x","00" -replace ":  "," "  -replace " ([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])",' $1 $2' | Out-File -Append -encoding ascii $outFile
 }
